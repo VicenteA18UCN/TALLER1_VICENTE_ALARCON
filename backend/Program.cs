f79c86f951047fe309a9e52e7481e39015c947b9
VicenteA18UCN <105085389+VicenteA18UCN@users.noreply.github.com>
2023-11-02 19:03:34 -0300

    feat(database): add models and configure database

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,6 @@
+using backend.Src.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite("Data Source=Dumbo.db")); ;
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

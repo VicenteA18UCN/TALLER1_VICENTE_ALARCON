50bce4eb095fc0f13ec424d96c01f6b63d7fe321
Vicente Alarcón <vicente.alarcon@alumnos.ucn.cl>
2023-11-21 15:32:32 -0300

    feat: added expiration token verification

--- frontend/src/app/router/RequireAuth.tsx
+++ frontend/src/app/router/RequireAuth.tsx
@@ -1,10 +1,19 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectId } from "../../features/auth/adminSlice";
+import { logout, selectExp, selectId } from "../../features/auth/adminSlice";
 import Navbar from "../layout/Navbar";
+import { useDispatch } from "react-redux";
 
 export default function RequireAuth() {
   const userId = useSelector(selectId);
+  const exp = useSelector(selectExp);
+  const now = Math.floor(Date.now() / 1000);
+  const dispatch = useDispatch();
+  if (exp && now > exp) {
+    dispatch(logout());
+    return <Navigate to="/login" />;
+  }
+
   if (userId) {
     return (
       <>
